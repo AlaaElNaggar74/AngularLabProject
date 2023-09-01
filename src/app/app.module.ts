a256105b9269206e0142a757f143ccc370e1105d
alaaelnaggar72 <elnaggar@gmail.com>
2023-09-01 15:23:07 +0300

    angularLabTest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { AbouteComponent } from './aboute/aboute.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    AbouteComponent,
+    SkillsComponent,
+    PortfolioComponent,
+    FooterComponent
+    
   ],
   imports: [
     BrowserModule,
